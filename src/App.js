9b9c1b0c3a178dd87cbdb5ac65b3abf1bc777724
Shi Tian <shitian007@hotmail.com>
2020-01-15 17:43:41 +0800

    Add SearchAppBar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/SearchAppBar';
+import Person from './components/Person'
+import Conference from './components/Conference'
+import Organization from './components/Organization'
+
+export default App;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Base />
     </div>
   );
 }
 
-export default App;
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>HOME</div>
+    )
+  }
+}
+
+class Base extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: ''
+    }
+  }
+
+  displayInfo = () => {
+    if (this.state.display == 'Person') {
+      return <Person />
+    } else if (this.state.display == 'Organization') {
+      return <Organization />
+    } else if (this.state.display == 'Conference') {
+      return <Conference />
+    } else {
+      return <Home />
+    }
+  };
+
+
+  render() {
+    return (
+      <div>
+        <SearchAppBar />
+        {this.displayInfo()}
+      </div>
+    )
+  }
+}
