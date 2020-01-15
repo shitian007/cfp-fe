308b4bf5e12399f10cafb6ccb22cda7744359f5d
Shi Tian <shitian007@hotmail.com>
2020-01-15 18:29:42 +0800

    Add Person/Conference/Org components

---
+++ src/components/Conference.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Conference() {
+    return (
+        <div>
+            Conference Info
+        </div>
+    )
+}
---
+++ src/components/Organization.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Organization() {
+    return (
+        <div>
+            Organization Info
+        </div>
+    )
+}
