9faa6985e148177e6820c028b47f8bdeaefc4bf0
Shi Tian <shitian007@hotmail.com>
2020-01-16 14:46:29 +0800

    Add jsonify_results

--- db.py
+++ db.py
@@ -12,6 +12,18 @@ class SearchQueries:
     org_search = lambda search_val: "SELECT id, name FROM Organizations WHERE name LIKE \'%{}%\' GROUP BY name LIMIT 30".format(search_val)
     conf_search = lambda search_val: "SELECT id, title FROM WikicfpConferences WHERE title LIKE \'%{}%\' GROUP BY title LIMIT 30".format(search_val)
 
+def jsonify_results(results, result_type):
+    """ Given list of results as List[Tuple], jsonifies each entry
+
+    Args:
+        results (List[Tuple]): List of tuples retrieved from database
+        result_type (string): Either of person / organization / conference
+
+    Returns:
+        [type]: [description]
+    """
+    return [{'type': result_type, 'id': result[0], 'text': result[1]} for result in results]
+
 @app.route('/home')
 @cross_origin()
 def hello():
@@ -23,9 +35,10 @@ def search():
     search_val = request.args.get('search_val')
     with sqlite3.connect(db_filepath) as cnx:
         cur = cnx.cursor()
-        persons = cur.execute(SearchQueries.person_search(search_val)).fetchall()
-        orgs = cur.execute(SearchQueries.org_search(search_val)).fetchall()
-        confs = cur.execute(SearchQueries.conf_search(search_val)).fetchall()
+        persons = jsonify_results(cur.execute(SearchQueries.person_search(search_val)).fetchall(), 'person')
+        orgs = jsonify_results(cur.execute(SearchQueries.org_search(search_val)).fetchall(), 'org')
+        confs = jsonify_results(cur.execute(SearchQueries.conf_search(search_val)).fetchall(), 'conf')
+        results = persons + orgs + confs
         return {
             'results': persons + orgs + confs
             }
