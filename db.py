f85c34f9f742bc9ac77be14b0c627413e56c6c50
Shi Tian <shitian007@hotmail.com>
2020-01-18 01:08:55 +0800

    Move jsonify methods to class

--- db.py
+++ db.py
@@ -6,39 +6,54 @@ app = Flask(__name__)
 cors = CORS(app)
 db_filepath = '/Users/shitian/Github/cfp-mining/crawls/03_jan/full_dl_flair.db'
 
-def jsonify_search_results(results, result_type):
-    """ Given list of results as List[Tuple], jsonifies each entry
+class Jsonifier:
+    @staticmethod
+    def id_name(results, result_type):
+        return [{
+            'type': result_type,
+            'id': result[0],
+            'text': result[1]
+            } for result in results]
 
-    Args:
-        results (List[Tuple]): List of tuples retrieved from database
-        result_type (string): Either of person / organization / conference
+    @staticmethod
+    def person_org(person_orgs):
+        return [{
+            'person_id': po[0],
+            'name': po[1],
+            'org_id': po[2],
+            'org': po[3],
+        } for po in person_orgs]
 
-    Returns:
-        [type]: [description]
-    """
-    return [{
-        'type': result_type,
-        'id': result[0],
-        'text': result[1]
-          } for result in results]
+    @staticmethod
+    def conf_persons(persons):
+        return [{
+            'person_id': person[0],
+            'name': person[1],
+            'org_id': person[2],
+            'org': person[3],
+            'role': person[4]
+            } for person in persons]
 
-def jsonify_conf_persons(persons):
-    return [{
-        'person_id': person[0],
-        'name': person[1],
-        'org_id': person[2],
-        'org': person[3],
-        'role': person[4]
-        } for person in persons]
 
 class SearchQueries:
-    # Generic Search
-    person_search = lambda search_val: "SELECT id, name FROM Persons WHERE name LIKE \'%{}%\' GROUP BY name LIMIT 30".format(search_val)
-    org_search = lambda search_val: "SELECT id, name FROM Organizations WHERE name LIKE \'%{}%\' GROUP BY name LIMIT 30".format(search_val)
-    conf_search = lambda search_val: "SELECT id, title FROM WikicfpConferences WHERE title LIKE \'%{}%\' GROUP BY title LIMIT 30".format(search_val)
+    # Generic Searches
+    person_search = lambda search_val, num: "SELECT id, name FROM Persons WHERE name LIKE \'%{}%\' GROUP BY name LIMIT {}".format(
+        search_val, num)
+
+    person_org_search = lambda search_val, num: "SELECT p.id, p.name, o.id, o.name FROM Persons p\
+                                            JOIN PersonOrganization po ON po.person_id=p.id\
+                                            JOIN Organizations o ON po.org_id=o.id WHERE p.name LIKE \'%{}%\' GROUP BY p.name LIMIT {}".format(
+                                                search_val, num)
+
+    org_search = lambda search_val, num: "SELECT id, name FROM Organizations WHERE name LIKE \'%{}%\' GROUP BY name LIMIT {}".format(search_val, num)
+
+    conf_search = lambda search_val, num: "SELECT id, title FROM WikicfpConferences WHERE title LIKE \'%{}%\' GROUP BY title LIMIT {}".format(search_val, num)
+
     # Conference Lookup
     conf_title = lambda conf_id: "SELECT title FROM WikicfpConferences WHERE id={}".format(conf_id)
+
     conf_pages = lambda conf_id: "SELECT url FROM ConferencePages WHERE conf_id={}".format(conf_id)
+
     conf_persons = lambda conf_id: "SELECT p.id, p.name, o.id, o.name, pr.role_type\
         FROM Persons p\
         JOIN PersonRole pr ON p.id=pr.person_id\
@@ -51,16 +66,17 @@ class SearchQueries:
 def hello():
     return "Home"
 
-@app.route('/search')
+@app.route('/autocomplete_search')
 @cross_origin()
-def search():
+def autocomplete_search():
+    max_results = 30
     max_search_length = 300
     search_val = request.args.get('search_val')
     with sqlite3.connect(db_filepath) as cnx:
         cur = cnx.cursor()
-        persons = jsonify_search_results(cur.execute(SearchQueries.person_search(search_val)).fetchall(), 'person')
-        orgs = jsonify_search_results(cur.execute(SearchQueries.org_search(search_val)).fetchall(), 'org')
-        confs = jsonify_search_results(cur.execute(SearchQueries.conf_search(search_val)).fetchall(), 'conf')
+        persons = Jsonifier.id_name(cur.execute(SearchQueries.person_search(search_val, max_results)).fetchall(), 'person')
+        orgs = Jsonifier.id_name(cur.execute(SearchQueries.org_search(search_val, max_results)).fetchall(), 'org')
+        confs = Jsonifier.id_name(cur.execute(SearchQueries.conf_search(search_val, max_results)).fetchall(), 'conf')
         results = list(filter(lambda r: len(r['text']) < max_search_length, persons + orgs + confs))
     return {
         'results': results
