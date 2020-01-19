6f0ddf3eece2402d2b88132a74d25bf219409eae
Shi Tian <shitian007@hotmail.com>
2020-01-19 16:51:13 +0800

    Fix JOIN for search results retrieval

--- db.py
+++ db.py
@@ -43,8 +43,8 @@ class SearchQueries:
         search_val, num)
 
     person_org_search = lambda search_val, num: "SELECT p.id, p.name, o.id, o.name FROM Persons p\
-                                            JOIN PersonOrganization po ON po.person_id=p.id\
-                                            JOIN Organizations o ON po.org_id=o.id WHERE p.name LIKE \'%{}%\' GROUP BY p.name LIMIT {}".format(
+                                            LEFT JOIN PersonOrganization po ON po.person_id=p.id\
+                                            LEFT JOIN Organizations o ON po.org_id=o.id WHERE p.name LIKE \'%{}%\' GROUP BY p.name LIMIT {}".format(
                                                 search_val, num)
 
     org_search = lambda search_val, num: "SELECT id, name FROM Organizations WHERE name LIKE \'%{}%\' GROUP BY name LIMIT {}".format(search_val, num)
