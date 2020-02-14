20bc19d5bfc2cdb376d565c7727dbccfe6d48e4c
Shi Tian <shitian007@hotmail.com>
2020-02-14 14:58:33 +0800

    Add display of Conference by iteration and series on Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-import { Paper, Tabs, Tab } from '@material-ui/core';
+import { Button, Paper, Tabs, Tab } from '@material-ui/core';
 import { Grid, Box } from '@material-ui/core';
 import { Link, withRouter } from 'react-router-dom';
 import { backendIP, personIssueURL, newConferenceIssueURL, updateConferenceIssueURL } from './constants';
@@ -11,6 +11,7 @@ class Home extends React.Component {
     super(props);
     this.mounted = false;
     this.state = {
+      series: [],
       confs: [],
       persons: [],
       orgs: [],
@@ -41,10 +42,11 @@ class Home extends React.Component {
       .then((responseJson) => {
         if (this.mounted) {
           this.setState({
+            series: responseJson.series,
             confs: responseJson.confs,
             persons: responseJson.persons,
             orgs: responseJson.orgs,
-            tabDisplay: <TopConferences confs={responseJson.confs} />
+            tabDisplay: <TopConferences confs={responseJson.confs} series={responseJson.series} />
           });
           this.props.setLoadingState(false);
         }
@@ -57,7 +59,7 @@ class Home extends React.Component {
     });
     if (value === 0) {
       this.setState({
-        tabDisplay: <TopConferences confs={this.state.confs} />
+        tabDisplay: <TopConferences confs={this.state.confs} series={this.state.series} />
       });
     } else if (value === 1) {
       this.setState({
@@ -93,15 +95,17 @@ class Home extends React.Component {
           </Grid>
         </Grid>
         <Grid container justify="center">
-          <Box style={{ margin: 20, width: 800, fontSize: 14 }}>
+          <Box align="left" style={{ margin: 20, width: 800, fontSize: 14 }}>
             <div> <b>NOTE</b> </div>
-            Most data on CFP-Mining is mined from individual conference call-for-papers and the verity of the information hence cannot be guaranteed.
+            Most data on CFP-Mining is mined from individual conference call-for-papers and the accuracy and completeness of the
+            information hence cannot be guaranteed.
             The code for data mining from conferences can be found at&nbsp;
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/shitian007/cfp-mining">cfp-mining</a>&nbsp;
             and the code for website at&nbsp;
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/shitian007/cfp-fe">cfp-fe</a>.<br/>
-            <Grid container>
-              <br/>For lapses or inaccuracy of any data, please request for corresponding updates as follows:
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/shitian007/cfp-fe">cfp-fe</a>.
+            PRs, comments and suggestions are highly welcome.<br/>
+            <Grid style={{marginTop: 10}} container>
+              <u>For lapses or inaccuracy of any data, please request for corresponding updates as follows:</u>
               <div>
                 <b>To ADD or UPDATE Person information: </b> please submit a new issue&nbsp;
                 <a target="_blank" rel="noopener noreferrer" href={personIssueURL}>here</a>
@@ -123,27 +127,73 @@ class Home extends React.Component {
 }
 
 class TopConferences extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayBySeries: false,
+      displayByIteration: true
+    }
+  }
+
+  setDisplayBy = (displayByType) => {
+    this.setState({
+      displayBySeries: displayByType === 'series',
+      displayByIteration: displayByType === 'iteration',
+    });
+  }
+
   render() {
-    let confs = this.props.confs;
+    let headers = []
+    let tableContent = []
+    if (this.state.displayByIteration) {
+      headers.push(['Conference Iteration', 'Year', 'Score'].map(headerName => {
+        return <TableCell key={headerName} align="left">{headerName}</TableCell>
+      }));
+      tableContent = this.props.confs.map(conf => (
+        <TableRow key={conf.id} align="left">
+          <TableCell> <Link to={'/conf/' + conf.id}> {conf.title} </Link> </TableCell>
+          <TableCell> {conf.year} </TableCell>
+          <TableCell> {conf.score} </TableCell>
+        </TableRow>
+      ))
+    } else {
+      headers.push(['Conference Series', 'Score'].map(headerName => {
+        return <TableCell key={headerName} align="left">{headerName}</TableCell>
+      }));
+      tableContent = this.props.series.map(series => (
+        <TableRow key={series.id} align="left">
+          <TableCell> {series.text} </TableCell>
+          <TableCell> {series.score} </TableCell>
+        </TableRow>
+      ));
+    }
+
     return (
       <Grid item>
-        <Grid style={{ marginTop: 20 }} container justify="space-between">
+        <Button
+          onClick={() => this.setDisplayBy('iteration')}
+          size="small" variant="outlined" color="primary"
+          disabled={this.state.displayByIteration}
+          style={{margin: 10}} >
+          By Iteration
+        </Button>
+        <Button
+          onClick={() => this.setDisplayBy('series')}
+          size="small" variant="outlined" color="primary"
+          disabled={this.state.displayBySeries}
+          style={{margin: 10}} >
+          By Series
+        </Button>
+        <Grid container justify="space-between">
           <Table size='small'>
             <TableHead>
               <TableRow style={{ background: 'lightgrey' }}>
-                <TableCell align="left">Conference Name</TableCell>
-                <TableCell align="left">Year</TableCell>
-                <TableCell align="left">Score</TableCell>
+                {headers}
               </TableRow>
             </TableHead>
             <TableBody>
-              {confs.map(conf => (
-                <TableRow key={conf.id} align="left">
-                  <TableCell> <Link to={'/conf/' + conf.id}> {conf.title} </Link> </TableCell>
-                  <TableCell> {conf.year} </TableCell>
-                  <TableCell> {conf.score} </TableCell>
-                </TableRow>
-              ))}
+              {tableContent}
             </TableBody>
           </Table>
         </Grid>
